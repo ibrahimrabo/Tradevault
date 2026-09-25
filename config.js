796136baf1fbd7513a910d2ce602f09ede031b94
ibrahimrabo <imuhammadrabo@gmail.com>
2026-09-25 13:51:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ Only put the Supabase project URL and anon/public key here.
 Never put a service_role key here.
 */
 window.TRADEVAULT_CONFIG = {
-  supabaseUrl: "YOUR_REAL_SUPABASE_API_URL",
-  supabaseAnonKey: "YOUR_REAL_PUBLISHABLE_KEY"
+  supabaseUrl: "https://pddakyvytwhptmtrdrcz.supabase.co/rest/v1/",
+  supabaseAnonKey: "sb_publishable_Yap3rOykCvN70fh7xBP50Q_Ua-iJZ6l"
 };
